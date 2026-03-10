88e4d8774ec9eb2058515a8c8e5aa64aae98bfda
linkearn51-source <linkearn51@gmail.com>
2026-03-10 23:52:15 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+FORCE_GROUP = os.getenv("FORCE_GROUP")
+
+DATABASE_CHANNEL = int(os.getenv("DATABASE_CHANNEL"))
