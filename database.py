725ae624ed98016b94953f24ba8fdefb994e3c89
linkearn51-source <linkearn51@gmail.com>
2026-03-10 23:50:49 +0700

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from config import MONGO_URI
-
-client = MongoClient(MONGO_URI)
-db = client["filestore"]
-
-files = db["files"]
-links = db["links"]
-users = db["users"]
